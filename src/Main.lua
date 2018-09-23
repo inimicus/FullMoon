c389130a0f74b6768e867a36f7adb32eaf0e4936
Garret Jennings <gjennings@me.com>
2018-09-23 16:26:26 -0700

    Bump to v1.1.1

--- src/Main.lua
+++ src/Main.lua
@@ -7,7 +7,7 @@
 -- -----------------------------------------------------------------------------
 MOON            = {}
 MOON.name       = "FullMoon"
-MOON.version    = "1.1.0"
+MOON.version    = "1.1.1"
 MOON.dbVersion  = 1
 MOON.slash      = "/moon"
 MOON.prefix     = "[moon] "
